b5d2c34dce7098002bd6121ec6d7ac169b46ece6
Gregory PASSENHEIM <dev.passenheim@gmail.com>
2022-06-01 00:51:01 -0400

    Add files via upload

--- extInitScript.js
+++ extInitScript.js
@@ -1,6 +1,6 @@
 console.log("1) Chargement du script 'extInitScript.js'.");
 
 // Defini les scripts a ajouter.
-export const JSFilesToAdd = new Array("https://www.gstatic.com/charts/loader.js",
+const JSFilesToAdd = new Array("https://www.gstatic.com/charts/loader.js",
 	"https://raw.githubusercontent.com/Gregory-PASSENHEIM/TalanGoodies/main/addListboxesEval.js", 
 	"https://raw.githubusercontent.com/Gregory-PASSENHEIM/TalanGoodies/main/addRadarGraph.js");
