6d69ac843e9cdaf99cb8968fae2b85e7fd89c659
Gregory PASSENHEIM <dev.passenheim@gmail.com>
2022-06-01 01:59:29 -0400

    Add files via upload

--- extInitScript.js
+++ extInitScript.js
@@ -1,6 +1,7 @@
 "use strict";
 console.log("1) Chargement du script 'extInitScript.js'.");
 
+/*
 // Defini les scripts a ajouter.
 const JSFilesToAdd = new Array(
 	"https://www.gstatic.com/charts/loader.js",
@@ -19,3 +20,25 @@ for (let index = 0; index < JSFilesToAdd.length; ++index) {
 	document.head.appendChild(addJSFile);
 	console.log("  => " + element + " added");
 }
+*/
+
+console.log("loaderJS to add");
+var loaderJS = document.createElement('script');
+loaderJS.setAttribute('src','https://www.gstatic.com/charts/loader.js');
+loaderJS.setAttribute('type','text/javascript');
+document.head.appendChild(loaderJS);
+console.log("  => loaderJS added");
+
+console.log("addListboxEval to add");
+var addListboxEval = document.createElement('script');
+addListboxEval.setAttribute('src','https://raw.githubusercontent.com/Gregory-PASSENHEIM/TalanGoodies/main/addListboxesEval.js');
+addListboxEval.setAttribute('type','text/javascript');
+document.head.appendChild(addListboxEval);
+console.log("  => addListboxEval added");
+
+console.log("addRadarGraph to add");
+var addRadarGraph = document.createElement('script');
+addRadarGraph.setAttribute('src','https://raw.githubusercontent.com/Gregory-PASSENHEIM/TalanGoodies/main/addRadarGraph.js');
+addRadarGraph.setAttribute('type','text/javascript');
+document.head.appendChild(addRadarGraph);
+console.log("  => addRadarGraph added");
